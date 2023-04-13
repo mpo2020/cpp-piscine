e95129030bd1bca1e7fbb49abcddb46493e16490
Marlon <mpolisse@student.42.rio>
2023-04-13 18:24:09 -0300

    cpp00-ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctype.h>
+
+std::string upperString(std::string str)
+{
+	int len = str.length();
+	
+	for (int i = 0; i < len; i++)
+	{
+		str[i] = std::toupper(str[i]);
+	}
+	foreach(toupper(str), str.begin(), str.end())
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		std::cout << upperString(argv[i]);
+		if(i == argc -1)
+			std::cout << std::endl;
+	}
+
+	return (0);
+}

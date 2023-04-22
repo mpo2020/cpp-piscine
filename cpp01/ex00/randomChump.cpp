5a5801688eb3563eadde6550476021d7f02014cc
Marlon <mpolisse@student.42.rio>
2023-04-22 13:04:06 -0300

    ex00 terminado

---
+++ cpp01/ex00/randomChump.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpolisse <mpolisse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 10:51:22 by mpolisse          #+#    #+#             */
+/*   Updated: 2023/04/22 13:02:13 by mpolisse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void randomChump( std::string name )
+{
+	Zombie *chump =	newZombie(name);
+	chump->announce();
+	delete chump;
+}

86ff04bea34e98be58a1fb460939555cae1e7817
Marlon <mpolisse@student.42.rio>
2023-04-27 14:15:28 -0300

    ex04 completo

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,42 +6,62 @@
 /*   By: mpolisse <mpolisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:01:48 by mpolisse          #+#    #+#             */
-/*   Updated: 2023/04/25 18:28:20 by mpolisse         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:08:01 by mpolisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SedIsForLosers.hpp"
 
 
-bool validation(int argc, char **argv)
-{
-	if (argc != 4)
+bool validation(int argc, char **argv, std::string fileContent, std::string firstArgument)
+{	
+	if (argc != 4 || firstArgument.empty() == 1) 
 	{
 		std::cout << "Error number of argumement" << std::endl;
 		return (false);
-	}
-	
-	if (access(argv[1], F_OK | R_OK | W_OK) != 0)
+	}	
+	else if (access(argv[1], F_OK | R_OK | W_OK) != 0)
 	{
 		std::cout << "File error" << std::endl;
+		return (false);
+	}
+	else if (fileContent.find(firstArgument) == std::string::npos)
+	{
+		std::cout << "text not found" << std::endl;
 		return (false);
 	}
 	return (true);
 }
 
-int open_file(std::string argument)
+std::string open_file(std::string file)
 {
 	std::ifstream fs;
 	std::string line;
 	std::string fileContent;
-	fs.open(argument);
+	fs.open(file);
 	while(getline(fs, line))
 		fileContent += line + '\n';
-	
+	fs.close();
+	return (fileContent);
+}
 
-	std::cout << fileContent;
-	//return (fs);
-	return(1);
+void writeFile(std::string file, std::string modifiedFile)
+{
+	std::ofstream fs;
+	std::string newFile;
+	newFile = file + ".replace";
+	fs.open(newFile);
+	fs << modifiedFile;
+	fs.close();
+}
+
+std::string findReplaceText(std::string fileContent, std::string firstArgument, std::string secondArgument)
+{
+	int startCharacter = fileContent.find(firstArgument);
+	int len = firstArgument.length();
+	std::string modifiedFile = fileContent.erase(startCharacter, len);
+	modifiedFile = fileContent.insert(startCharacter, secondArgument);
+	return (modifiedFile);	
 }
 
 int	main(int argc, char **argv)
@@ -49,20 +69,18 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	std::string arguments[3];
-	
-	if (validation(argc, argv) == false)
+	std::string fileContent;
+
+	fileContent = open_file(argv[1]);
+	if (validation(argc, argv, fileContent, argv[2]) == false)
 	{
 		return (0);
 	}
-
-	for (int i = 0; i < 3; ++i )
+	while (fileContent.find(argv[2]) != std::string::npos)
 	{
-		arguments[i] = argv[i + 1];
+		fileContent = findReplaceText(fileContent, argv[2], argv[3]);
+		writeFile(argv[1], fileContent);
 	}
-	
-	open_file(arguments[0]);
-	
-	
 	
 	
 	return (0);

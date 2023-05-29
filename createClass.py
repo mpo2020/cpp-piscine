f61818f68540e4ea77f09e6a46039cdee9b547d3
Marlon <marlonpo@gmail.com>
2023-05-29 14:48:08 -0300

    correcao create class

--- createClass.py
+++ createClass.py
@@ -11,22 +11,47 @@ def create_canonical_class(class_name):
 
     colors = input('Want colors to be defined: ')
     if colors == 'yes':
-        header_output += '# define DEFAULT "033[0m"\n'
-        header_output += '# define RED "033[31m"\n'
-        header_output += '# define GREEN "033[32m"\n'
-        header_output += '# define Yellow "033[33m"\n'
-        header_output += '# define BLUE "033[34m"\n'
-        header_output += '# define CYAN "033[36m"\n\n'
+        header_output += '# define DEFAULT "\033[0m"\n'
+        header_output += '# define RED "\033[31m"\n'
+        header_output += '# define GREEN "\033[32m"\n'
+        header_output += '# define YELLOW "\033[33m"\n'
+        header_output += '# define BLUE "\033[34m"\n'
+        header_output += '# define CYAN "\033[36m"\n\n'
 
 
     header_output += f'class {class_name} {{\n'
-    header_output += 'public:\n'
     
+    
+    # Private attributes
+    private_attributes = input('Enter private attributes separated by commas: ')
+    if private_attributes:
+        header_output += 'private:\n'
+        for attribute in private_attributes.split(','):
+            header_output += f'\t{attribute.strip()};\n'
+
+    # Protected attributes
+    protected_attributes = input('Enter protected attributes separated by commas: ')
+    if protected_attributes:
+        header_output += 'protected:\n'
+        for attribute in protected_attributes.split(','):
+            header_output += f'\t{attribute.strip()};\n'
+    
+    # Public attributes
+    header_output += 'public:\n'
+
+    public_attributes = input('Enter public attributes separated by commas: ')
+    if public_attributes:
+        for attribute in public_attributes.split(','):
+            header_output += f'\t{attribute.strip()};\n'
+        header_output += '\n'
+    
+    
+
     # Default constructor
     header_output += f'\t{class_name}();\n'
     
     # Copy constructor
-    header_output += f'\t{class_name}(const {class_name}& other);\n'
+    header_output += f'\t{class_name}(const {class_name}& original);\n'
     
     # Assignment operator
     header_output += f'\t{class_name}& operator=(const {class_name}& other);\n'
@@ -34,32 +59,9 @@ def create_canonical_class(class_name):
     # Destructor
     header_output += f'\t~{class_name}();\n'
     
-    header_output += 'private:\n'
+ 
     
-    # Private attributes
-    private_attributes = input('Enter private attributes separated by commas: ')
-    if private_attributes:
-        for attribute in private_attributes.split(','):
-            header_output += f'\t{attribute.strip()};\n'
-    
-    header_output += 'protected:\n'
-    
-    # Protected attributes
-    protected_attributes = input('Enter protected attributes separated by commas: ')
-    if protected_attributes:
-        header_output += '\t// Protected attributes\n'
-        for attribute in protected_attributes.split(','):
-            header_output += f'\t{attribute.strip()};\n'
-    
-    header_output += 'public:\n'
-    
-    # Public attributes
-    public_attributes = input('Enter public attributes separated by commas: ')
-    if public_attributes:
-        header_output += '\t// Public attributes\n'
-        for attribute in public_attributes.split(','):
-            header_output += f'\t{attribute.strip()};\n'
-    
+
     header_output += '};\n\n'
     header_output += f'#endif'
     
@@ -67,20 +69,20 @@ def create_canonical_class(class_name):
     cpp_output = f'#include "{class_name}.hpp"\n\n'
     
     cpp_output += f'{class_name}::{class_name}() {{\n'
-    cpp_output += f'\tstd::cout << "{class_name} default constructor called" << std::endl;\n'
+    cpp_output += f'\tstd::cout << CYAN << "{class_name}" << DEFAULT << GREEN << " default constructor called" << DEFAULT << std::endl;\n'
     cpp_output += '}\n\n'
     
     cpp_output += f'{class_name}::{class_name}(const {class_name}& original) {{\n'
-    cpp_output += f'\tstd::cout << "{class_name} copy constructor called" << std::endl;\n'
+    cpp_output += f'\tstd::cout << CYAN << "{class_name}" << DEFAULT << GREEN << " copy constructor called" << DEFAULT << std::endl;\n'
     cpp_output += '}\n\n'
     
     cpp_output += f'{class_name}& {class_name}::operator=(const {class_name}& other) {{\n'
-    cpp_output += f'\tstd::cout << "{class_name} assingment operator called" << std::endl;\n'
+    cpp_output += f'\tstd::cout << CYAN << "{class_name}" << DEFAULT << BLUE << " assingment operator called" << DEFAULT << std::endl;\n'
     cpp_output += '\treturn (*this);\n'
     cpp_output += '}\n\n'
     
     cpp_output += f'{class_name}::~{class_name}() {{\n'
-    cpp_output += f'\tstd::cout << "{class_name} destructor called" << std::endl;\n'
+    cpp_output += f'\tstd::cout << CYAN << "{class_name}" << DEFAULT << RED << " destructor called" << DEFAULT << std::endl;\n'
     cpp_output += '}\n\n'
     
     return header_output, cpp_output
